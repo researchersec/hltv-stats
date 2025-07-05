68a60d08950a2d448a3474694ef6e95a73929c87
researchersec <109282836+researchersec@users.noreply.github.com>
2025-07-06 01:27:08 +0200

    Update datagatherer_results.py

--- datagatherer_results.py
+++ datagatherer_results.py
@@ -9,7 +9,6 @@ import requests
 import zoneinfo
 import tzlocal
 
-# Set up logging configuration
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
