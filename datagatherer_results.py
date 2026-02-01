9028fd6645ce2d9380d7ba3a5942e825d01e4a62
researchersec <109282836+researchersec@users.noreply.github.com>
2026-02-01 17:33:52 +0100

    Decrease MAX_RUNTIME_SECONDS to 5 minutes
    Reduced MAX_RUNTIME_SECONDS from 300 minutes to 5 minutes.

--- datagatherer_results.py
+++ datagatherer_results.py
@@ -12,7 +12,7 @@ import tzlocal
 
 # ================== CONFIG ================== #
 
-MAX_RUNTIME_SECONDS = 60 * 300
+MAX_RUNTIME_SECONDS = 60 * 5 #60 * 300
 MAX_RESULTS_OFFSET = 23000
 STATE_FILE = "scrape_state.json"
 RESULTS_FILE = "results.json"
