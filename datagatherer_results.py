23fb9d9afe75044caff2b5d4fefbad93444631de
researchersec <109282836+researchersec@users.noreply.github.com>
2026-02-02 05:39:00 +0100

    Increase MAX_RUNTIME_SECONDS and MAX_RESULTS_OFFSET

--- datagatherer_results.py
+++ datagatherer_results.py
@@ -12,8 +12,8 @@ import tzlocal
 
 # ================== CONFIG ================== #
 
-MAX_RUNTIME_SECONDS = 60 * 5
-MAX_RESULTS_OFFSET = 200
+MAX_RUNTIME_SECONDS = 60 * 300
+MAX_RESULTS_OFFSET = 23000
 
 STATE_FILE = "scrape_state.json"
 RESULTS_FILE = "results.json"
